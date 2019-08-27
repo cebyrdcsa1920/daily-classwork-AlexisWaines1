73195e888443274cd0d4d7dc61b3a163791eec51
AlexisWaines1 <alexiswaines@yahoo.com>
2019-08-27 13:55:57 +0000

    Create a constructor for Student

---
+++ week3/Student.java
@@ -0,0 +1,16 @@
+public class Student
+{
+    private double gpa;
+    private String studentID;
+    private String name;
+    private int age;
+
+    public Student(String name, String studentID, int age)
+    {
+        this.gpa = 0.0;
+        this.name =name;
+        this.studentID = studentId;
+        this.age = age;
+
+    }
+}

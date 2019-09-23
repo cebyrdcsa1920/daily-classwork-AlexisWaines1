212607bae8dfbc036067b4546caf99ce44b2e911
AlexisWaines1 <alexiswaines@yahoo.com>
2019-09-23 14:15:55 +0000

    Create temperature class

---
+++ week6/Family.java
@@ -0,0 +1,19 @@
+public class Family
+{
+  private numOfMembers;
+  private numOfKids;
+  private race;
+}
+    public Family()
+    {
+        numOfMembers = 4;
+        numOfKids = 2;
+        race = african desent;
+    }
+    public Family (String numOfMembers, int numOfKids,double race)
+    {
+        this. numOfMembers = numOfMembers;
+        this.numOfKids = numOfKids;
+        this.race = race;
+
+    }
---
+++ week6/Phone.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+
+public class Phone
+{
+    private String type;
+    private Color color;
+    private numApps;
+    private numUnreadMessages;
+}
+    public Phone()
+    {
+        type = smartphone;
+        color= Color.jetblack;
+        numApps = 12;
+        numUnreadMessages = 89;
+    }
+
+    public Phone (String type, Color color, int numApps, double numUnreadMessages )
+    {
+        this.type = model;
+        this. color = color;
+        this. numApps = numApps;
+        this. numUnreadMessages = numUnreadMessages;
+    }
